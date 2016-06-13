8aa51f028770fb92229f26a3f7bb37a5a314e4ad
JianyuZhao93 <planetzjy@gmail.com>
2016-06-13 13:25:26 +0200

    Submitted version

--- Exercise2.m
+++ Exercise2.m
@@ -13,30 +13,29 @@
 %
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clearvars;
+function [dopt,mClsErr,ConMt] = Exercise2(dmax)
 
-dmax = 10;
+    TestLb = loadMNISTLabels('t10k-labels.idx1-ubyte');
+    num = size(TestLb,1);
 
-TestLb = loadMNISTLabels('t10k-labels.idx1-ubyte');
-num = size(TestLb,1);
+    %% Change values to d to find the optimal one
+    ClsErr = zeros(dmax,1);
+    PreLb = zeros(num,dmax);
+    for l=1:dmax
+        [ClsErr(l),PreLb(:,l)] = BaysianClassifier(l);
+    end
 
-%% Change values to d to find the optimal one
-ClsErr = zeros(dmax,1);
-PreLb = zeros(num,dmax);
-for l=1:dmax
-    [ClsErr(l),PreLb(:,l)] = BaysianClassifier(l);
+    %% Visualization
+    figure;
+    plot(1:dmax,100*ClsErr,'LineWidth',1);
+    xlabel('d value'); ylabel({'Classification Error','(in percentage)'});
+    title('Classification Error Plot');
+
+    %% Optimal value of d and its confusion matrix
+    [mClsErr,dopt] = min(ClsErr);
+    PreOpt = PreLb(:,dopt);
+    %[ErrOpt,PreOpt] = BaysianClassifier(dopt);
+    ConMt = confusionmat(TestLb,PreOpt);
+    helperDisplayConfusionMatrix(ConMt);
+
 end
-
-%% Visualization
-figure;
-plot(1:dmax,100*ClsErr,'LineWidth',1);
-xlabel('d value'); ylabel({'Classification Error','(in percentage)'});
-title('Classification Error Plot');
-
-%% Optimal value of d
-[mClsErr,dopt] = min(ClsErr);
-PreOpt = PreLb(:,dopt);
-%[ErrOpt,PreOpt] = BaysianClassifier(dopt);
-ConMt = confusionmat(TestLb,PreOpt);
-helperDisplayConfusionMatrix(ConMt);
-
---
+++ main.m
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
+% Handwritten digits classification using Baysian classifier
+% Main function
+% 
+% Author: Jianyu Zhao
+% Last revised: 13.06.2016
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clearvars;
+[D,CLSERR,ConMatrix] = Exercise2(60);
